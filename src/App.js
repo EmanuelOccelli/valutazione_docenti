653913e9658b848ad8c52ea8662f503d4dca1615
EmanuelOccelli <emanueloccelli@gmail.com>
2023-12-28 16:32:19 +0100

    pipi

--- src/App.js
+++ src/App.js
@@ -1,47 +1,51 @@
 
 import './App.css';
 
-import React, {useState} from 'react'
-let mail
+import React, {useState,useRef} from 'react'
+let mail=""
 let pass
 
 const App = () => {
-  const [miamail, setmiamail] = useState("")
-
-  const [psw, setpsw] = useState("")
-
+  const [miamail,setMiamail] = useState("")
   const leggimail = (param) => {
     mail=param.target.value
+    setMiamail(param.target.value)
   }
   const leggipsw = (param) => {
     pass=param.target.value
   }
   const visualizza = () =>{
     console.log("Benvenuto" +" "+ mail)
+   
+    return (
+      <p>Ciao {miamail}</p>
+    )
   }
+  
   return (
-    <div class="container bordo centra">
-      <div class="row">
-        <div class="col-12">
-          <div class="row">
-            <div class="col-12 bordo centra">Valutazione Docenti</div>
-            <div class="col-6 bordo centra">
+    <div className="container margcont  centra">
+      <div className="row bordo">
+        <div className="col-12">
+          <div className="row">
+            <div className="col-12  centra testo">Valutazione Docenti</div>
+            <div className="col-6 margine centra">
               Nome utente:
             </div>
-            <div class="col-6 bordo centra">
+            <div className="col-6 margine centra">
               <input type="text" placeholder="Inserisci Nome utente" onChange={leggimail}></input>
             </div>
-            <div class="col-6 bordo centra">
+            <div className="col-6 margine centra">
               Password:
             </div>
-            <div class="col-6 bordo centra">
+            <div className="col-6 margine centra">
               <input type="password" placeholder="Inserisci password" onChange={leggipsw}></input>
             </div>
-            <div class="col-12 bordo centra">
+            <div className="col-12 margine centra">
               <input type="button" onClick={visualizza} value="INVIA"></input>
             </div>
           </div>
         </div>
+        <div className="col-12 bordo centra">{visualizza()}</div>
       </div>
     </div>
   )
