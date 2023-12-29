53e63ecde54cd6939031fe4e971d7e25ebdc0964
StefanoGarnerone <144361274+StefanoGarnerone@users.noreply.github.com>
2023-12-29 14:14:14 +0100

    2.1

--- src/index.js
+++ src/index.js
@@ -1,21 +1,14 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from "./pages/Layout";
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
-import NoPage from "./pages/NonPage";
-import Login from "./pages/Login";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="contact" element={<Contact />} />
-          <Route path="*" element={<NoPage />} />
-          <Route path="Login" element={<Login />} />
-        </Route>
       </Routes>
     </BrowserRouter>
   );
